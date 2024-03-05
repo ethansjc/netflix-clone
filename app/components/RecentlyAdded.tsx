abe71ca936e217aa0d97365a297e11a17dbda868
ethansjc <ethansjcheong@gmail.com>
2024-03-05 18:00:16 -0500

    Created UI pages

---
+++ app/components/RecentlyAdded.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import prisma from "../utils/db";
+
+async function getData() {
+    const data = await prisma.movie.findMany({
+        select: {
+            id: true,
+            overview: true,
+            title: true,
+            WatchLists: true,
+            imageString: true,
+            videoSource: true,
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+        take: 4,
+    });
+
+    return data;
+}
+
+export default async function RecentlyAdded() {
+    const data = await getData();
+
+    return (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-8 gap-6">
+            {data.map((movie) => (
+                <div key={movie.id} className="relative h-48">
+                    <Image
+                        src={movie.imageString}
+                        alt="Movie"
+                        width={500}
+                        height={400}
+                        className="rounded-sm absolute w-full h-full object-cover"
+                    />
+                </div>
+            ))}
+
+        </div>
+    )
+}
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,15 @@
+import MovieVideo from "../components/MovieVideo";
 import Navbar from "../components/Navbar";
+import RecentlyAdded from "../components/RecentlyAdded";
 
 export default function HomePage() {
     return (
-        <h1>
-            Hello from home.
-        </h1>
+
+        <div className="p-5 lg:p-0">
+            <MovieVideo />
+            <h1 className="text-3xl font-bold">Recently Added</h1>
+            <RecentlyAdded/>
+        </div>
+
     )
 }
