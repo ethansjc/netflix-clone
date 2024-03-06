7adc12aa152685c33fb68166f6e35a5a1a0b85f7
ethansjc <ethansjcheong@gmail.com>
2024-03-06 16:33:19 -0500

    Completed movie card component

--- app/components/RecentlyAdded.tsx
+++ app/components/RecentlyAdded.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import prisma from "../utils/db";
+import { MovieCard } from "./MovieCard";
 
 async function getData() {
     const data = await prisma.movie.findMany({
@@ -9,7 +10,10 @@ async function getData() {
             title: true,
             WatchLists: true,
             imageString: true,
-            videoSource: true,
+            youtubeString: true,
+            age: true,
+            release: true,
+            duration: true,
         },
         orderBy: {
             createdAt: "desc",
@@ -34,6 +38,32 @@ export default async function RecentlyAdded() {
                         height={400}
                         className="rounded-sm absolute w-full h-full object-cover"
                     />
+
+                    <div className="h-60 relative z-10 w-full transform transition duration-500 hover:scale-125 opacity-0 hover:opacity-100">
+                        <div className="bg-gradient-to-b from-transparent via-black/50 to-black z-10 w-full h-full rounded-lg flex items-center justify-center">
+                            <Image
+                                src={movie.imageString}
+                                alt="Movie"
+                                width={800}
+                                height={800}
+                                className="absolute w-full h-full -z-10 rounded-lg object-cover"
+                            />
+                            <MovieCard
+                                movieId={movie.id}
+                                overview={movie.overview}
+                                title={movie.title}
+                                watchListId={movie.WatchLists[0]?.id}
+                                youtubeUrl={movie.youtubeString}
+                                watchList={movie.WatchLists.length > 0 ? true : false}
+                                key={movie.id}
+                                age={movie.age}
+                                time={movie.duration}
+                                year={movie.release }
+
+                            />
+                        </div>
+                    </div>
+
                 </div>
             ))}
 
