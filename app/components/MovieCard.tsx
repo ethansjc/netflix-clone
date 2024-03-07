5019c40fecd219e05e054434bed9b7b2013628b0
ethansjc <ethansjcheong@gmail.com>
2024-03-06 22:16:34 -0500

    Completed watchlist functionality

--- app/components/MovieCard.tsx
+++ app/components/MovieCard.tsx
@@ -4,6 +4,8 @@ import { Button } from "@/components/ui/button";
 import { Heart, PlayCircle } from "lucide-react";
 import PlayVideoModal from "./PlayVideoModal";
 import { useState } from "react";
+import { addTowatchlist, deleteFromWatchlist } from "../action";
+import { usePathname } from "next/navigation";
 
 interface iAppProps {
     title: string;
@@ -20,23 +22,28 @@ interface iAppProps {
 export function MovieCard({ movieId, title, overview, watchList, watchListId, youtubeUrl, age, time, year }: iAppProps) {
 
     const [open, setOpen] = useState(false);
+    const pathName = usePathname();
 
     return (
         <>
-            <button onClick={()=> setOpen(true)} className="mt-14">
+            <button onClick={() => setOpen(true)} className="mt-14">
                 <PlayCircle className="h-20 w-20" />
             </button>
 
             <div className="right-5 top-5 absolute z-10">
                 {watchList ? (
-                    <form>
+                    <form action={deleteFromWatchlist}>
+                        <input type="hidden" name="watchlistId" value={watchListId} />
+                        <input type="hidden" name="pathname" value={pathName} />
                         <Button variant="outline" size="icon">
                             <Heart className="w-4 h-4 text-red-500" />
                         </Button>
                     </form>
 
                 ) : (
-                    <form>
+                    <form action={addTowatchlist}>
+                        <input type="hidden" name="movieId" value={movieId} />
+                        <input type="hidden" name="pathname" value={pathName} />
                         <Button variant="outline" size="icon">
                             <Heart className="w-4 h-4" />
                         </Button>
